a89e67c5ef40149bcabf590fe5041e15e956bdcb
無資 蕭 <4862094zxd@gmail.com>
2023-12-26 20:51:14 +0800

    big

--- 41243254專題/41243254-2048.c
+++ 41243254專題/41243254-2048.c
@@ -10,16 +10,18 @@ void up();
 void down();
 void left();
 void right();
+void random();
+void end();
 int number[4][4] = { 0 };
-int win2048[9] = { 8,16,32,64,128,256,512,1024,2048 };
+int win2048[11] = {2,4,8,16,32,64,128,256,512,1024,2048 };
 int main()
 {
-	int i;
-	random();
-	system("cls");
-	game();
+	firstrandom();
 	gamebox();
 	play();
+	random();
+	system("cls");
+	gamebox();
 	return 0;
 }
 
@@ -42,7 +44,6 @@ void gamebox()
 	printf("¢x%4d  ¢x%4d  ¢x%4d  ¢x%4d  ¢x	 \n", number[3][0], number[3][1], number[3][2], number[3][3]);
 	printf("¢x      ¢x      ¢x      ¢x      ¢x  \n");
 	printf("¢|¢w¢w¢w¢w¢w¢w¢r¢w¢w¢w¢w¢w¢w¢r¢w¢w¢w¢w¢w¢w¢r¢w¢w¢w¢w¢w¢w¢}\n");
-	
 }
 
 void firstrandom()
@@ -92,14 +93,164 @@ void play()
 
 void up()
 {
-	int i, j;
-	for (i = 3; i != 0; i--)
+	int i, j, k;
+	for (i = 0; i < 4; i++) 
 	{
-		for ( j = 3; j !=0; j--)
+		for (j = 1; j < 4; j++) 
 		{
-			number[j][i];
-
+			if (number[j][i] != 0) 
+			{
+				for (k = j; k > 0 && number[k - 1][i] == 0; k--) 
+				{
+					number[k - 1][i] = number[k][i];
+					number[k][i] = 0;
+				}
+			}
 		}
 	}
+	for (i = 0; i < 4; i++) 
+	{
+		for (j = 0; j < 3; j++) 
+		{
+			if (number[j][i] == number[j + 1][i]) 
+			{
+				for (k = 0; k < 11; k++) 
+				{
+					if (number[j][i] == win2048[k]) 
+					{
+						number[j][i] = win2048[k + 1];
+						number[j + 1][i] = 0;
+						break; 
+					}
+				}
+			}
+		}
+	}
+}
 
+void down()
+{
+	int i, j, k;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 2; j >= 0; j--)
+		{
+			if (number[j][i] != 0)
+			{
+				for (k = j; k < 3 && number[k + 1][i] == 0; k++)
+				{
+					number[k + 1][i] = number[k][i];
+					number[k][i] = 0;
+				}
+			}
+		}
+	}
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 3; j > 0; j--)
+		{
+			if (number[j][i] == number[j - 1][i])
+			{
+				for (k = 0; k < 11; k++)
+				{
+					if (number[j][i] == win2048[k])
+					{
+						number[j-1][i] = win2048[k + 1];
+						number[j][i] = 0;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+void left()
+{
+	int i, j, k;
+	for (i = 0; i < 4; i++) 
+	{
+		for (j = 1; j < 4; j++) 
+		{
+			if (number[i][j] != 0) 
+			{
+				for (k = j; k > 0 && number[i][k - 1] == 0; k--) 
+				{
+					number[i][k - 1] = number[i][k];
+					number[i][k] = 0;
+				}
+			}
+		}
+	}
+	for (i = 0; i < 4; i++) 
+	{
+		for (j = 0; j < 3; j++) 
+		{
+			if (number[i][j] == number[i][j + 1]) 
+			{
+				for (k = 0; k < 11; k++) {
+					if (number[i][j] == win2048[k]) 
+					{
+						number[i][j] = win2048[k + 1];
+						number[i][j + 1] = 0;
+						break; 
+					}
+				}
+			}
+		}
+	}
+}
+
+void right()
+{
+	int i, j, k;
+	for (i = 0; i < 4; i++) 
+	{
+		for (j = 2; j >= 0; j--) 
+		{
+			if (number[i][j] != 0) 
+			{
+				for (k = j; k < 3 && number[i][k + 1] == 0; k++) 
+				{
+					number[i][k + 1] = number[i][k];
+					number[i][k] = 0;
+				}
+			}
+		}
+	}
+	for (i = 0; i < 4; i++) 
+	{
+		for (j = 3; j > 0; j--) 
+		{
+			if (number[i][j] == number[i][j - 1]) 
+			{
+				for (k = 0; k < 11; k++) 
+				{
+					if (number[i][j] == win2048[k]) 
+					{
+						number[i][j - 1] = win2048[k + 1];
+						number[i][j] = 0;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+void random()
+{
+	int i,j,b,a[2] = {2,4};
+	srand((unsigned)time(NULL));
+	i = rand() % 4;
+	j = rand() % 4;
+	b = rand() % 2;
+	if (number[i][j]==0)
+	{
+		number[i][j] = a[b];
+	}
+	else
+	{
+		random();
+	}
 }
